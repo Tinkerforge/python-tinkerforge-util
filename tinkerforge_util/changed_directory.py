1ae8763b4ef5b2e3c3912be13f9d3d1cea73bef8
Matthias Bolte <matthias@tinkerforge.com>
2024-07-05 21:24:23 +0200

    Fix copyright year

--- tinkerforge_util/changed_directory.py
+++ tinkerforge_util/changed_directory.py
@@ -1,6 +1,6 @@
 #
 # Tinkerforge Util
-# Copyright (C) 2016 Matthias Bolte <matthias@tinkerforge.com>
+# Copyright (C) 2024 Matthias Bolte <matthias@tinkerforge.com>
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
